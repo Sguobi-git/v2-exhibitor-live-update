874a977bb992cea36161c7a934d9c0ef491e9e15
Souhail Guobi <guobisouhail@gmail.com>
2025-07-17 17:46:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -276,6 +276,11 @@ def get_stats():
     
     return jsonify(stats)
 
+# if __name__ == '__main__':
+#     import os
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port, debug=False)
+
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
