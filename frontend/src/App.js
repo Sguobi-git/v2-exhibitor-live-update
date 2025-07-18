a5ee2469026d75f6888ad12156b89cd9291a0e34
Souhail Guobi <guobisouhail@gmail.com>
2025-07-18 22:12:07 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -536,7 +536,6 @@ function App() {
                     <span>Expo Convention Contractors</span>
                   </span>
                   <span className="text-xs md:text-sm text-gray-500">Live Order Tracking</span>
-                  )}
                 </div>
               </div>
             </div>
